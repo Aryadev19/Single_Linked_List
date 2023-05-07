fcacf63014f501c795125a37099ff28a9e05de99
Piyush Aryaprakash <piyusharyaprakash12345@gmail.com>
2023-05-07 17:18:16 +0530

    add ssll class constructor

---
+++ Singly_Linked_List.js
@@ -0,0 +1,20 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = null;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+    if (this.head === null) {
+    }
+  }
+}
